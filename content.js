8875eac6893f3bf115f4721c321077718f60ff98
Roberto Magale <roberto.magale@gmail.com>
2016-08-06 00:48:25 +0100

    Changes cursor style to match the original when reenabled

--- content.js
+++ content.js
@@ -1,26 +1,29 @@
 'use strict'
-String.prototype.contains = function(substring) {
-  return this.indexOf(substring) > -1;
+String.prototype.contains = function (substring) {
+    return this.indexOf(substring) > -1;
 }
 
-function executeOnButton(condition, modify) {
-  var aTags = document.getElementsByTagName("button");
+var bamEnabled = false;
+var buttonStyle;
 
-  for (var i = 0; i < aTags.length; i++) {
-    if (condition(aTags[i])) {
-      modify(aTags[i]);
+function executeOnButton(condition, modify) {
+    var aTags = document.getElementsByTagName("button");
+
+    for (var i = 0; i < aTags.length; i++) {
+        if (condition(aTags[i])) {
+            modify(aTags[i]);
+        }
     }
-  }
 }
 
 function isLiveButton(button) {
-  return button.textContent.contains('Promote to live');
+    return button.textContent.contains('Promote to live');
 }
-var bamEnabled = false;
-executeOnButton(isLiveButton, function(button) {
-  button.disabled = true;
-  button.style.cursor = 'not-allowed';
-  button.className = 'button red';
+
+executeOnButton(isLiveButton, function (button) {
+    button.disabled = true;
+    button.style.cursor = 'not-allowed';
+    button.className = 'button red';
 });
 
 /*
@@ -28,22 +31,23 @@ This method has to have this particular signature (3 params) and it will be exec
 every time the content script receives a call (a request)
 */
 function changeBamStatus(request, sender, callback) {
-  if (request.action === 'bam') {
-    bamEnabled = !bamEnabled;
-    if (bamEnabled) {
-      executeOnButton(isLiveButton, function(button) {
-        button.disabled = false;
-        button.style.cursor = 'default';
-      });
+    if (request.action === 'bam') {
+        bamEnabled = !bamEnabled;
+        if (bamEnabled) {
+            executeOnButton(isLiveButton, function (button) {
+                button.disabled = false;
+                button.style.cursor = originalButtonCursorStyle;
+            });
+        } else {
+            executeOnButton(isLiveButton, function (button) {
+                button.disabled = true;
+                originalButtonCursorStyle = button.style.cursor;
+                button.style.cursor = 'not-allowed';
+            });
+        }
     } else {
-      executeOnButton(isLiveButton, function(button) {
-        button.disabled = true;
-        button.style.cursor = 'not-allowed';
-      });
+        console.error('Action not defined');
     }
-  } else {
-    console.error('Action not defined');
-  }
 }
 
 /*
@@ -55,23 +59,23 @@ chrome.extension.onMessage.addListener(changeBamStatus);
 // Inform the background page that
 // this tab should have a page-action
 chrome.runtime.sendMessage({
-  from: 'content',
-  subject: 'showPageAction'
+    from: 'content',
+    subject: 'showPageAction'
 });
 
 // Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function(msg, sender, response) {
-  // First, validate the message's structure
-  if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
-    // Collect the necessary data
-    // (For your specific requirements `document.querySelectorAll(...)`
-    //  should be equivalent to jquery's `$(...)`)
-    var domInfo = {
-      bamEnabled: bamEnabled
-    };
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+    // First, validate the message's structure
+    if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+        // Collect the necessary data
+        // (For your specific requirements `document.querySelectorAll(...)`
+        //  should be equivalent to jquery's `$(...)`)
+        var domInfo = {
+            bamEnabled: bamEnabled
+        };
 
-    // Directly respond to the sender (popup),
-    // through the specified callback */
-    response(domInfo);
-  }
+        // Directly respond to the sender (popup),
+        // through the specified callback */
+        response(domInfo);
+    }
 });
