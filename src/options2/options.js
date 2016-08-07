85bd4b13a6bc11fcddd58f65c1ff320ccf54e8e6
Roberto Magale <roberto.magale@gmail.com>
2016-08-07 22:17:00 +0100

    Added working table

--- src/options2/options.js
+++ src/options2/options.js
@@ -1,32 +1,22 @@
-$(function () {
-    $("td").click(function () {
+$(document).ready(function () {
+    var i = 1;
+    $("#add_row").click(function () {
+        $('#opt' + i).html(
+            "<td>" + (i + 1) + "</td>" +
+            "<td><input name='name" + i + "' type='text' placeholder='Name' class='form-control input-md' /> </td>" +
+            "<td><input name='url" + i + "' type='text' placeholder='Rule Url' class='form-control input-md'></td>" +
+            "<td><input  name='selector" + i + "' type='text' placeholder='Mobile' class='form-control input-md'></td>" +
+            "<td><input  name='disable" + i + "' type='checkbox'</td>" +
+            "<td><input  name='color" + i + "' type='checkbox'</td>");
 
-        var firstChild = $(this).children().first();
-        if ($(this).children().length === 0) {
-            var OriginalContent = $(this).text();
-
-            $(this).addClass("cellEditing");
-            $(this).html("<input class='form-control' type='text' value='" + OriginalContent + "' />");
-            $(this).children().first().focus();
-
-            $(this).children().first().keypress(function (e) {
-                if (e.which == 13 || e.wich == 27) {
-                    var newContent = $(this).val();
-                    $(this).parent().text(newContent);
-                    $(this).parent().removeClass("cellEditing");
-                }
-            });
-            $(this).children().first().focusout(function () {
-                var newContent = $(this).val();
-                $(this).parent().text(newContent);
-                $(this).parent().removeClass("cellEditing");
-            });
-
-
-            $(this).children().first().blur(function () {
-                $(this).parent().text(OriginalContent);
-                $(this).parent().removeClass("cellEditing");
-            });
+        $('#optionsTable').append('<tr id="opt' + (i + 1) + '"></tr>');
+        i++;
+    });
+    $("#delete_row").click(function () {
+        if (i > 1) {
+            $("#opt" + (i - 1)).html('');
+            i--;
         }
     });
+
 });
