ce2ce189a02271ceb5246feb62af9d09ec1ab898
Roberto Magale <roberto.magale@gmail.com>
2016-08-14 21:25:55 +0100

    Fixed a bug in background

--- src/background/background.js
+++ src/background/background.js
@@ -11,7 +11,7 @@ chrome.tabs.onUpdated.addListener(function (id, info, tab) {
     chrome.storage.sync.get({
         'disabledContent': []
     }, function (options) {
-
+        validOptions = [];
         options.disabledContent.forEach(function (item) {
             if (tab.url.indexOf(item.url) > -1) {
                 validOptions.push(item);
@@ -22,8 +22,7 @@ chrome.tabs.onUpdated.addListener(function (id, info, tab) {
     if (validOptions.length > 0) {
         chrome.pageAction.show(tab.id);
         chrome.tabs.executeScript(tab.id, {
-            file: 'src/content/content.js',
-            runAt: 'documentEnd'
+            file: 'src/content/content.js'
         }, function () {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError.message);
